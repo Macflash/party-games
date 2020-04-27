7c09b71ad20063805edd181453544c497a51acf6
Macflash <the.alex.breyfogle@gmail.com>
2020-04-27 11:59:29 -0700

    update online api parsing and handling

--- src/generic/onlineApi.ts
+++ src/generic/onlineApi.ts
@@ -1,20 +1,23 @@
-import { IGenericGameApi, ICreateGameContract } from "./apis";
+import { IGenericGameApi, ICreateGameContract, IJoinGameResponse } from "./apis";
 import axios from "axios";
 import { ServerGameObject } from "./types";
 
-function encodeObjectData(game: ServerGameObject):ServerGameObject {
-    return {...game, data: JSON.stringify(game.objectData)}
+function encodeObjectData(game: ServerGameObject): ServerGameObject {
+    return { ...game, data: JSON.stringify(game.objectData) }
 }
 
-function decodeData(game: ServerGameObject):ServerGameObject {
-    return {...game, objectData: JSON.parse(game.data)}
+function decodeObjectData(game: ServerGameObject): ServerGameObject {
+    return { ...game, objectData: JSON.parse(game.data) }
 }
+
+const handleCreateRequest = (request: ICreateGameContract) => ({ ...request, game: encodeObjectData(request.game) });
+const handleCreateAndJoinResponse = (r: { data: ICreateGameContract | IJoinGameResponse }) => ({ ...r.data, game: decodeObjectData(r.data.game) });
 
 export const OnlineApi: IGenericGameApi = {
     Lobby: {
         GetAll: () => axios.get("/Games/GetGames").then(r => r.data),
-        Create: request => axios.post<ICreateGameContract>("/Games/Create", {...request, game:encodeObjectData(request.game) }).then(r => ({...r.data, game: decodeData(r.data.game)})),
-        Join: request => axios.post("/Games/Join", request).then(r => r.data),
+        Create: request => axios.post<ICreateGameContract>("/Games/Create", handleCreateRequest(request)).then(handleCreateAndJoinResponse),
+        Join: request => axios.post<IJoinGameResponse>("/Games/Join", request).then(handleCreateAndJoinResponse),
     },
     Play: {
 
