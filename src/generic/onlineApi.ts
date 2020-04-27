8129a2733a0cccd75fdef8366d6011ce491c4ffb
Macflash <the.alex.breyfogle@gmail.com>
2020-04-27 11:55:31 -0700

    decode and encode on CREATE

--- src/generic/onlineApi.ts
+++ src/generic/onlineApi.ts
@@ -1,11 +1,19 @@
-import { IGenericGameApi } from "./apis";
+import { IGenericGameApi, ICreateGameContract } from "./apis";
 import axios from "axios";
 import { ServerGameObject } from "./types";
+
+function encodeObjectData(game: ServerGameObject):ServerGameObject {
+    return {...game, data: JSON.stringify(game.objectData)}
+}
+
+function decodeData(game: ServerGameObject):ServerGameObject {
+    return {...game, objectData: JSON.parse(game.data)}
+}
 
 export const OnlineApi: IGenericGameApi = {
     Lobby: {
         GetAll: () => axios.get("/Games/GetGames").then(r => r.data),
-        Create: request => axios.post("/Games/Create", request).then(r => r.data),
+        Create: request => axios.post<ICreateGameContract>("/Games/Create", {...request, game:encodeObjectData(request.game) }).then(r => ({...r.data, game: decodeData(r.data.game)})),
         Join: request => axios.post("/Games/Join", request).then(r => r.data),
     },
     Play: {
